652148cfd83707bb118a8040bf5f6d31235b6384
kpalanivelrajan <66392449+kpalanivelrajan@users.noreply.github.com>
2020-06-20 00:05:06 +0100

    Update set_env.sh

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME= palanivel
-export POSTGRES_PASSWORD= pvrajan1984
+export POSTGRES_PASSWORD= mypassword
 export POSTGRES_HOST= cdr-palanivel.cwyy2pyqolav.eu-west-1.rds.amazonaws.com
 export POSTGRES_DB= cdr-palanivel
 export AWS_BUCKET= cdr-909587919238-dev
